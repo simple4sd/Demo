71ce31434f0fd5670fea29dc660c1896f863b023
Bruce <tinywood@126.com>
2018-07-28 14:40:57 +0800

    增加对json数组长度解析

--- cJSON/config_parse.c
+++ cJSON/config_parse.c
@@ -4,34 +4,41 @@
 
 int parse_policy(char *policy)
 {
-	if (policy == NULL)
-		return -1;
+    int ret = -1;
+    if (policy == NULL)
+        return ret;
 
-	const cJSON *keyfile   = NULL;
-	const cJSON *file_path = NULL;
-	cJSON *policy_json = cJSON_Parse(policy);
-	if (policy_json == NULL) {
-		const char *error_ptr = cJSON_GetErrorPtr();
-		if (error_ptr != NULL)
-			printf("parse error %s\n", error_ptr);
-		goto out;
-	}
+    const cJSON *keyfile   = NULL;
+    const cJSON *file_path = NULL;
+    cJSON *policy_json = cJSON_Parse(policy);
+    if (policy_json == NULL) {
+        const char *error_ptr = cJSON_GetErrorPtr();
+        if (error_ptr != NULL)
+            printf("parse error %s\n", error_ptr);
+        goto out;
+    }
 
-	keyfile = cJSON_GetObjectItemCaseSensitive(policy_json, "keyfile");
-	cJSON_ArrayForEach(file_path, keyfile) {
-		if (cJSON_IsString(file_path)) {
-			printf("file path is %s\n", file_path->valuestring);
-		}
-	}
+    keyfile = cJSON_GetObjectItemCaseSensitive(policy_json, "keyfile");
+    if (keyfile == NULL)
+        goto out;
+
+    int size = cJSON_GetArraySize(keyfile);
+    printf("size = %d\n", size);
+    cJSON_ArrayForEach(file_path, keyfile) {
+        if (cJSON_IsString(file_path)) {
+            printf("file path is %s\n", file_path->valuestring);
+        }
+    }
+    ret = 0;
 out:
-	cJSON_Delete(policy_json);
-	return 0;
+    cJSON_Delete(policy_json);
+    return ret;
 }
 
 int main()
 {
-	char policy[] = "{\"keyfile\":[\"/etc/init.d\", \"/tmp/abc\"]}";
-	printf("policy = %s\n", policy);
-	parse_policy(policy);
-	return 0;
+    char policy[] = "{\"keyfile\":[\"/etc/init.d\", \"/tmp/abc\"]}";
+    printf("policy = %s\n", policy);
+    parse_policy(policy);
+    return 0;
 }
